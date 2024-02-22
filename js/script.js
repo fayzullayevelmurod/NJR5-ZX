4e0f36180a95990b8e6e4dcdbae2a09cd8d38c31
SardorQobilov <sardorqobilov555@gmail.com>
2024-02-22 16:27:56 +0500

    project finished

--- js/script.js
+++ js/script.js
@@ -40,11 +40,7 @@ $(document).ready(function () {
     $('.parent-modal').removeClass('show');
 
   })
-  $('.send-data').click(function () {
-    $('.parent-modal').removeClass('show');
-    $('.parent-moda-two').addClass('show');
 
-  })
   $('.modal__close-two').click(function () {
     $('.parent-moda-two').removeClass('show');
 
@@ -56,5 +52,31 @@ $(document).ready(function () {
   $('.close-btn').click(function () {
     $('.nav').removeClass('active')
   })
+
+  $("#submit").click(function (e) {
+    e.preventDefault();
+
+    var formFields = ['name', 'email', 'phone', 'organizatioin', 'adress'];
+    var clickTime = new Date().getTime();
+
+    for (var i = 0; i < formFields.length; i++) {
+      var field = $("#" + formFields[i]);
+      var value = field.val();
+
+      if (value.length == "") {
+        field.focus().addClass('danger');
+        return false;
+      } else {
+        field.removeClass('danger');
+      }
+    }
+
+    var currentTime = new Date().getTime();
+    if (currentTime - clickTime < 1000) {
+      $('.parent-modal').removeClass('show');
+      $('.parent-moda-two').addClass('show');
+    }
+  });
+
 
 });
