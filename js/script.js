a12ac792a2917a9430aa3c04912294134ebbd628
SardorQobilov <sardorqobilov555@gmail.com>
2024-02-22 05:56:27 +0500

    asd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   var swiper = new Swiper(".hero-swiper", {
-    slidesPerView: 2,
-    spaceBetween: 40,
+    slidesPerView: 1,
+    spaceBetween: 100,
     pagination: {
       el: ".swiper-pagination",
     },
@@ -27,4 +27,23 @@ $(document).ready(function () {
   $(".accordion-header").click(function () {
     $(this).toggleClass("active").next(".accordion-content").slideToggle().parent().siblings().find(".accordion-content").slideUp().prev().removeClass("active");
   });
+
+  // modal
+  $('.open-modal').click(function () {
+    $('.parent-modal').addClass('show');
+    document.body.style.overflow = 'hidden';
+  })
+  $('.modal-close').click(function () {
+    $('.parent-modal').removeClass('show');
+    document.body.style.overflow = 'auto';
+  })
+  $('.send-data').click(function () {
+    $('.parent-modal').removeClass('show');
+    $('.parent-moda-two').addClass('show');
+    document.body.style.overflow = 'hidden';
+  })
+  $('.modal__close-two').click(function () {
+    $('.parent-moda-two').removeClass('show');
+    document.body.style.overflow = 'auto';
+  })
 });
